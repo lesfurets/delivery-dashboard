17c34c53a4da4ac4ff3a48bb346bf43045fb0542
Geoffrey Berard <geoffrey.berard@gmail.com>
2015-11-09 15:49:47 +0100

    Delivery-dashboard    * Ajout Dashboard Data

---
+++ app/config/app-config-data.js
@@ -0,0 +1,12 @@
+var RAW_DATA_URL = '1XOQbSWW5X_xU9wjp_f5LtFmMUaLZn2SBGm2uiyb7g-c';
+
+var RAW_DATA_COL = {
+    PROJECT: 0,
+    REF: 1,
+    EFFORT: 2,
+    VALUE: 3,
+    CREATION: 4,
+    ANALYSIS: 5,
+    DEVELOPMENT: 6,
+    RELEASE: 7
+};
