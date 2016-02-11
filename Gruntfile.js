ab294c6719dde9b5a67c74f4076c95a38b6d3a79
Geoffrey Berard <geoffrey.berard@gmail.com>
2016-02-11 20:10:57 +0100

    Delivery dashboard    * LESS file segmentation    * LiveReload set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,49 @@
-module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-less");
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-less");
 
-  grunt.initConfig({
-    less: {
-      development: {
-        options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "styles/css/main.css": "styles/less/main.less"
+                }
+            }
         },
-        files: {
-          "styles/css/main.css": "styles/less/main.less"
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']
+            },
+            main: {
+                src: 'styles/css/*.css'
+            }
+        },
+        watch: {
+            less: {
+                files: ['styles/less/**/*.less'],
+                tasks: ['default'],
+            },
+            css: {
+                files: ['styles/css/main.css'],
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: ['html/**/*.html'],
+                options: {
+                    livereload: true
+                }
+            }
         }
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']
-      },
-      main: {
-        src: 'styles/css/*.css'
-      }
-    },
-    watch: {
-      styles: {
-        files: ['styles/less/**/*.less'],
-        tasks: ['default'],
-        options: {
-          livereload: true,
-          nospawn: true
-        }
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('default', ['less', 'autoprefixer']);
-  grunt.registerTask('w', ['less', 'autoprefixer', 'watch']);
+    grunt.registerTask('default', ['less', 'autoprefixer']);
+    grunt.registerTask('w', ['less', 'autoprefixer', 'watch']);
 };
